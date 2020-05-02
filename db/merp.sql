2c570188d1489daa7758a9c1e2b38b7508faad4f
Christopher Yocum <cyocum@gmail.com>
2020-05-02 22:53:31 +0100

    Started on AT-3 2-Handed attack table

--- db/merp.sql
+++ db/merp.sql
@@ -455,5 +455,44 @@ insert into one_handed_concussion_weapons_attack_table
 	    (141, 145, 16, 6, 21, 7, 19, 7, 19, 7, 22, 7),
 	    (146, 150, 16, 7, 22, 7, 20, 7, 20, 7, 23, 7);
 
+drop table if exists two_handed_weapons_attack_table;
+
+create table two_handed_weapons_attack_table
+      	     (roll_value_min integer,
+       	      roll_value_max integer,
+	      plate unsigned tinyint,
+	      plate_crit_mod tinyint,
+	      chain unsigned tinyint,
+	      chain_crit_mod tinyint,
+	      rigid_leather unsigned tinyint,
+	      rigid_leather_crit_mod tinyint,
+	      soft_leather unsigned tinyint,
+	      soft_leather_crit_mod tinyint,
+	      none unsigned tinyint,
+	      none_crit_mod tinyint,
+    	      foreign key(plate_crit_mod) references criticals(critical_id),
+	      foreign key(chain_crit_mod) references criticals(critical_id),
+	      foreign key(rigid_leather_crit_mod) references criticals(critical_id),
+	      foreign key(soft_leather_crit_mod) references criticals(critical_id),
+	      foreign key(none_crit_mod) references criticals(critical_id));
+
+
+insert into two_handed_weapons_attack_table
+       	    (roll_value_min,
+       	    roll_value_max,
+	    plate,
+	    plate_crit_mod,
+	    chain,
+	    chain_crit_mod,
+	    rigid_leather,
+	    rigid_leather_crit_mod,
+	    soft_leather,
+	    soft_leather_crit_mod,
+	    none,
+	    none_crit_mod)
+	    values
+	    (1, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8),
+	    (9, 55, 0, '', 0, '', 0, '', 0, '', 0, ''),
+	    (56, 60, 2, '', 0, '', 0, '', 0, '', 0, '');
 
 commit;
